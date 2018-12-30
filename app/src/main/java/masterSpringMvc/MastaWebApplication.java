e94d668ac7238ecb824c298f129a4e69f6467cbd
Jakub Dubański <jakub.dubanski@gmail.com>
2018-12-30 22:23:14 +0100

    [SpringBoot] Init.

---
+++ app/src/main/java/masterSpringMvc/MastaWebApplication.java
@@ -0,0 +1,14 @@
+package masterSpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastaWebApplication.class, args);
+	}
+
+}
+
